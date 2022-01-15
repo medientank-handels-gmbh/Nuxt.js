3679cae9a9d1e2b3956a9ddeea63c455f5d41673
thunder6230-work <84312829+thunder6230-work@users.noreply.github.com>
2022-01-15 12:25:19 +0100

    project init

---
+++ WebApi/Controllers/Home.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class Home : ControllerBase
+{
+    [HttpGet]
+    public string Index()
+    {
+        return "Hello World!";
+    }
+}
